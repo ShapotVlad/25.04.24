0feb8350bafe2b043c109996480f6a8c04023c46
Vladimir <vlnshapat@gmail.com>
2024-04-25 22:55:27 +0300

    add task1/добавили формат

--- sem3_task1/Program.cs
+++ sem3_task1/Program.cs
@@ -8,14 +8,14 @@
 // int[] array2 = new int[5] { 3, 6, 8, 3, 9 };
 // int[] array3 = new int[] { 3, 6, 8, 3, 9 };
 
-int[] array = {1, 3, 8, 19, 3, 8};
+int[] array = { 1, 3, 2, 19, 3, 12 };
 int num = 8;
 
 bool isExistNum = false;
 
 for (int i = 0; i < array.Length; i++)
 {
-    if(array[i] == num) 
+    if (array[i] == num)
     {
         isExistNum = true;
         break;
