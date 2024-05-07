2951cf07ff92296f130d988ed1d988f39ecc8306
Vladimir <vlnshapat@gmail.com>
2024-05-07 14:20:16 +0300

    add changes in task4

--- sem3_task4/Program.cs
+++ sem3_task4/Program.cs
@@ -11,10 +11,10 @@ num = Convert.ToInt32(Console.ReadLine());
 int numCopy = num;
 int count = 0;
 
-// счетает количество цифр в числе
-while (numCopy != 0)
-{
-    numCopy /= 10;
+// считаем количество цифр в числе
+
+while (numCopy != 0) // пока число не равно 0
+   { numCopy /= 10;  //numCopy = numCopy/10
     count++;
 }
 
@@ -23,12 +23,12 @@ int[] arr = new int[count]; // создаем массив на количест
 int index = 0; // индекс последнего элемента 
 while (num != 0)
 {
-    arr[index] = num % 10;
+    arr[index] = num % 10; //остаток от деления на 10
     num /= 10; // num = num / 10; 
     index++;
 }
 Console.Write("[");
-for (int i = 0; i < arr.Length; i++)
+for (int i = 0; i < arr.Length; i++)  
 {
     Console.Write($"{arr[i]} ");
 }
