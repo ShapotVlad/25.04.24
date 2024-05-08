02183e25d1cec5b05ccffa0045daa1fadec49dec
Vladimir <vlnshapat@gmail.com>
2024-05-08 18:34:07 +0300

    add homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,31 @@
+﻿//создать массив  из вещественных чисел с ненулевой дробной частью
+//найти разницу между максимальноым и минимальным элементами массива
+
+double[] array = new double[8] { 13.12, 4.22, 25.36, 34.78, 98.94, 9.69, 52.31, 73.63 };
+
+double max = array[0];
+double min = array[0];
+
+double difference = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+difference = max - min;
+
+Console.WriteLine($"вывод результата = {difference}");
+
+
