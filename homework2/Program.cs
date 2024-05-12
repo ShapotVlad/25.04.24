5707c068fb0697604d9cebfa998480624d6dd746
Vladimir <vlnshapat@gmail.com>
2024-05-12 19:25:28 +0300

    add homework2 test

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -14,3 +14,24 @@ for (int i = 0; i < array.Length; i++)
 }
 
  Console.WriteLine($"количество четных элементов в массиве = {count}");
+
+
+// // Подсчет количества четных элементов массива
+//     // numbers - массив, в котором ведется подсчет
+//     public static int CountEvenItems(int[] numbers)
+//     {
+//         int cnt = 0;
+//         for (int i = 0; i < numbers.Length; i++)
+//         {
+//             if (numbers[i] % 2 == 0)
+//             {
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+
+//     public static void PrintResult(int[] array)
+//     {
+//         Console.WriteLine(CountEvenItems(array));
+//     }
